4d68c40ce8be49042d5636ca37f2e00d3f370b4c
Simon Jentsch <simon@bojagi.io>
2021-01-17 23:58:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+ const parser = require('fast-xml-parser');
+const fetch = require('node-fetch');
+const AsciiTable = require('ascii-table')
+
+function germanEnglish(query) {
+  const table = new AsciiTable();
+
+  function addEntryToTable(entry) {
+    table.addRow(entry.side[0].words.word, entry.side[1].words.word);
+  }
+
+  function handleSectionEntry(item) {
+    if (Array.isArray(item.entry)) {
+      item.entry.map(addEntryToTable)
+    } else {
+      addEntryToTable(item.entry);
+    }
+  }
+
+  fetch(`https://dict.leo.org/dictQuery/m-vocab/ende/query.xml?lp=ende&lang=de&search=${query}&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=`)
+    .then(res => res.text())
+    .then(res => parser.parse(res))
+    .then(res => {
+      if (Array.isArray(res.xml.sectionlist.section)) {
+        return res.xml.sectionlist.section.map(handleSectionEntry)
+      } else {
+        return handleSectionEntry(res.xml.sectionlist.section);
+      }
+    })
+    .then(() => console.log(table.toString()))
+}
+
+germanEnglish(process.argv[2]);
