9c433765e6cb1e099bee224284e1f4ab02018e2c
Simon Jentsch <simon@bojagi.io>
2021-01-18 00:37:32 +0100

    Add copy functionality

--- index.js
+++ index.js
@@ -6,35 +6,71 @@ const AsciiTable = require('ascii-table')
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
+const inquirer = require('inquirer');
+
+function transformWord(word) {
+  if (Array.isArray(word)) {
+    return word.join('; ');
+  }
+
+  return word;
+}
 
 function queryLeo(langCode, query) {
-  const table = new AsciiTable();
-
-  function addEntryToTable(entry) {
-    table.addRow(entry.side[0].words.word, entry.side[1].words.word);
+  function transformEntry(entry) {
+    const leftWord = transformWord(entry.side[0].words.word);
+    const rightWord = transformWord(entry.side[1].words.word);
+    return [leftWord, rightWord];
   }
 
   function handleSectionEntry(item) {
     if (Array.isArray(item.entry)) {
-      item.entry.map(addEntryToTable)
+      return item.entry.map(transformEntry)
     } else {
-      addEntryToTable(item.entry);
+      return [transformEntry(item.entry)];
     }
   }
 
-  fetch(`https://dict.leo.org/dictQuery/m-vocab/${langCode}de/query.xml?lp=${langCode}de&lang=de&search=${query}&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=`)
+  return fetch(`https://dict.leo.org/dictQuery/m-vocab/${langCode}de/query.xml?lp=${langCode}de&lang=de&search=${query}&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=`)
     .then(res => res.text())
     .then(res => parser.parse(res))
     .then(res => {
       if (Array.isArray(res.xml.sectionlist.section)) {
-        return res.xml.sectionlist.section.map(handleSectionEntry)
+        return res.xml.sectionlist.section.flatMap(handleSectionEntry)
       } else {
-        return handleSectionEntry(res.xml.sectionlist.section);
+        return [handleSectionEntry(res.xml.sectionlist.section)];
       }
     })
-    .then(() => console.log(table.toString()))
+    
 }
 
 const lang = argv.lang || 'en';
 const query = argv._.join(' ');
-queryLeo(lang, query);
+
+if(argv.cp) {
+  queryLeo(lang, query).then(res => {
+    inquirer.prompt({
+      name: 'results',
+      type: 'list',
+      choices: res.map(([left, right]) => ({
+        value: left,
+        name: `${left} -> ${right}`,
+      }))
+    }).then(prompt => {
+      pbcopy(prompt.results);
+    });
+  })
+} else {
+  const table = new AsciiTable();
+  queryLeo(lang, query)
+    .then(res => {
+      return res.forEach(item => table.addRow(...item));
+    })
+    .then(() => console.log(table.toString()))
+}
+
+function pbcopy(data) {
+  // Only works on macOS
+  var proc = require('child_process').spawn('pbcopy'); 
+  proc.stdin.write(data); proc.stdin.end();
+}
